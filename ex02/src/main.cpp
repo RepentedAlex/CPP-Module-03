27955509c9055e9978870dfe68094178d0556e56
apetitco <apetitco@student.42.fr>
2025-06-18 15:45:52 +0200

    commented tests that testes previous requirements

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -13,26 +13,26 @@ do { \
 
 int main()
 {
-	{
-		std::cout << "### TESTING CLAPTRAP ###" << std::endl;
-		std::cout << BLUE "Constructing" RESET << std::endl;
-		ClapTrap a;
-		ClapTrap b("Cody");
-
-		std::cout << BLUE "Testing" RESET << std::endl;
-		a.attack("some other robot");
-		a.takeDamage(10);
-		a.takeDamage(10);
-		a.beRepaired(5);
-		a.attack("some other other robot");
-		b.beRepaired(3);
-		for (int i = 0; i < 12; i++)
-			b.attack("Cody-clone");
-		b.beRepaired(3);
-		std::cout << BLUE "Deconstructing" RESET << std::endl;
-	}
-
-	std::cout << std::endl << std::endl;
+	/*{*/
+	/*	std::cout << "### TESTING CLAPTRAP ###" << std::endl;*/
+	/*	std::cout << BLUE "Constructing" RESET << std::endl;*/
+	/*	ClapTrap a;*/
+	/*	ClapTrap b("Cody");*/
+	/**/
+	/*	std::cout << BLUE "Testing" RESET << std::endl;*/
+	/*	a.attack("some other robot");*/
+	/*	a.takeDamage(10);*/
+	/*	a.takeDamage(10);*/
+	/*	a.beRepaired(5);*/
+	/*	a.attack("some other other robot");*/
+	/*	b.beRepaired(3);*/
+	/*	for (int i = 0; i < 12; i++)*/
+	/*		b.attack("Cody-clone");*/
+	/*	b.beRepaired(3);*/
+	/*	std::cout << BLUE "Deconstructing" RESET << std::endl;*/
+	/*}*/
+	/**/
+	/*std::cout << std::endl << std::endl;*/
 
 	{
 		PRINT_SECTION_TITLE("### TESTING FRAGTRAP ###");
